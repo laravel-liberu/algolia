d59c54b307cba3a757b8506511dc027991f2b2b9
Vlad Chiovianu <vlad.chiovianu@earthlink.ro>
2021-08-12 11:09:29 +0300

    removes redundant migration, fixes upgrade

--- src/Upgrades/Settings.php
+++ src/Upgrades/Settings.php
@@ -2,14 +2,12 @@
 
 namespace LaravelEnso\Algolia\Upgrades;
 
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use LaravelEnso\Algolia\Models\Settings as Model;
 use LaravelEnso\Upgrade\Contracts\MigratesData;
-use LaravelEnso\Upgrade\Contracts\MigratesPostDataMigration;
 use LaravelEnso\Webshop\Models\Settings as WebshopSettings;
 
-class Settings implements MigratesData, MigratesPostDataMigration
+class Settings implements MigratesData
 {
     public function isMigrated(): bool
     {
@@ -25,14 +23,5 @@ class Settings implements MigratesData, MigratesPostDataMigration
             'secret' => $settings->algolia_secret,
             'enabled' => (bool) $settings->algolia_enabled,
         ])->save();
-    }
-
-    public function migratePostDataMigration(): void
-    {
-        Schema::table('webshop_settings', function (Blueprint $table) {
-            $table->dropColumn('algolia_app_id');
-            $table->dropColumn('algolia_secret');
-            $table->dropColumn('algolia_enabled');
-        });
     }
 }
