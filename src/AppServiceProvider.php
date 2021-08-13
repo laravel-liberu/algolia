249545a63fefc59905cfe85a675f88bdc90816b0
Vlad Chiovianu <vlad.chiovianu@earthlink.ro>
2021-08-13 14:00:18 +0300

    removes misplaced provider

--- src/AppServiceProvider.php
+++ src/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace LaravelEnso\Algolia;
 
 use Illuminate\Support\ServiceProvider;
-use LaravelEnso\Algolia\Http\Middleware\Integrations;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,11 +10,5 @@ class AppServiceProvider extends ServiceProvider
     {
         $this->loadRoutesFrom(__DIR__.'/../routes/api.php');
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-    }
-
-    public function register()
-    {
-        $this->app['router']
-            ->aliasMiddleware('integrations', Integrations::class);
     }
 }
